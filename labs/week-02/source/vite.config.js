51f86a28542348d6ac2d4148312e4b9d75b6f1b5
ItsPitxh <pitched.10121@gmail.com>
2026-07-23 16:05:56 +0700

    feat(week-02): migrate modern JavaScript lab

---
+++ labs/week-02/source/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+
+const repositoryName = "engse203-lab02-67543210061-7";
+
+export default defineConfig({
+  base: `./`,
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
