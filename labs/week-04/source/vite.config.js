ca36a9f6fa9e08651706e8208e260a9c44472df1
ItsPitxh <pitched.10121@gmail.com>
2026-08-08 22:31:19 +0700

    quick fix(week-04): quick fix)

--- labs/week-04/source/vite.config.js
+++ labs/week-04/source/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+
+
+const repositoryName = "engse203-lab02-67543210061-7";
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  base: `./`,
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
