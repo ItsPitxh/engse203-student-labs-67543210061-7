a72d93665aa9fd5f943d06f146dc0bc5df32a3d6
ItsPitxh <pitched.10121@gmail.com>
2026-08-13 13:41:27 +0700

    week-04(rebuild-pages) rebuilding pages from start

--- labs/week-04/source/vite.config.js
+++ labs/week-04/source/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // MUST match the format from Section 9:
-  base: '/engse203-student-labs-<student-id>/labs/week-04/', 
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  base: './',
 });
